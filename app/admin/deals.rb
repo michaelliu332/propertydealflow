55ba6a90e2973c7e42b6deaac3bb6f9f6b5791a5
Ratnam Raju <ratnamraju.v@nyros.in>
2012-02-10 09:12:39 +0530

    modifications deal

--- app/admin/deals.rb
+++ app/admin/deals.rb
@@ -1,17 +1,13 @@
 ActiveAdmin.register Deal do
   actions :all, :except => [:new, :create ]
   
- sidebar "sampe" do
      index do
- 
       column :content
-
       default_actions
     end
     form do |f|
       f.inputs :content 
       f.buttons
     end
- end
   
 end
